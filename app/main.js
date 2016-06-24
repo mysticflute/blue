88f782972a48ec06b3244e2f1e066fffb00ce78e
Nathan McWilliams <nmcwilliams@salesforce.com>
2016-06-24 00:37:48 -0400

    progress

--- app/main.js
+++ app/main.js
@@ -15,10 +15,7 @@ function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/app/index.html`);
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  win.loadURL(`file://${__dirname}/index.html`);
 
   // Emitted when the window is closed.
   win.on('closed', () => {
