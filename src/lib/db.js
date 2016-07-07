8e81a16f37d84b1c5395d3fc4975a7149a288f70
Nathan McWilliams <nmcwilliams@salesforce.com>
2016-07-07 17:54:37 -0400

    change nickname, db cleanup

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,75 +5,31 @@ const Q = require('Q');
 const path = require('path');
 const nedb = require('nedb');
 const electron = require('electron').remote;
-const Logger = require('./logger');
-const logger = new Logger();
-const dbPath = path.join(electron.app.getPath('userData'), 'appData');
+const dest = path.join(electron.app.getPath('userData'), 'appData');
 
 const cards = new nedb({
-  filename: path.join(dbPath, 'cards.db'),
+  filename: path.join(dest, 'cards.db'),
   autoload: true,
   timestampData: true
 });
 
 exports.getCards = function() {
-  let deferred = Q.defer();
-
-  cards.find({}, (err, all) => {
-    if (err) {
-      logger.warn('error retrieving all cards', err);
-      deferred.reject(err);
-    } else {
-      deferred.resolve(all);
-    }
-  });
-
-  return deferred.promise;
+  return Q.ninvoke(cards, 'find', {});
 };
 
 exports.newCard = function(record) {
-  let deferred = Q.defer();
-
-  logger.log('inserting new card into db');
-  cards.insert(record, (err, newCard) => {
-    if (err) {
-      logger.warn('error inserting new card', err);
-      deferred.reject(err);
-    }
-    logger.log('card successfully inserted', newCard);
-    deferred.resolve(newCard);
-  });
-
-  return deferred.promise;
+  console.log('inserting new card into db');
+  return Q.ninvoke(cards, 'insert', record);
 };
 
 exports.updateCard = function(id, updates) {
-  logger.log('updateCard', id, updates);
+  return Q.ninvoke(cards, 'update', { _id: id}, {$set: updates });
 };
 
 exports.removeCard = function(id) {
-  let deferred = Q.defer();
-
-  cards.remove({ _id: id }, {}, (err, numRemoved) => {
-    if (err) {
-      deferred.reject(err);
-    } else {
-      deferred.resolve(numRemoved);
-    }
-  });
-
-  return deferred.promise;
+  return Q.ninvoke(cards, 'remove', { _id: id }, {});
 };
 
 exports.removeAllCards = function() {
-  let deferred = Q.defer();
-
-  cards.remove({}, { multi: true }, (err, numRemoved) => {
-    if (err) {
-      deferred.reject(err);
-    } else {
-      deferred.resolve(numRemoved);
-    }
-  });
-
-  return deferred.promise;
+  return Q.ninvoke(cards, 'remove', {}, { multi: true });
 };
